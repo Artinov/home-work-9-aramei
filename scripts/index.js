01b9670d44f055aa1f8c0cedc2bcf0fb345a57b1
Apas <pasichnyk89@gmail.com>
2017-02-23 23:32:03 +0200

    add random class

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 var ball = document.querySelector(".discoball");
 
+var classes = [".here", ".goes", ".our", ".awesome", ".disco", ".ball"];
+
 function goDisco() {
-	//do your disco magic here !
+	var random = Math.floor((Math.random() * 5));
+
+	ball.setAttribute("class", classes[random]);
 }
 
 setInterval(goDisco,300);
+
+// var els = document.querySelectorAll('.publish_0');
+// for (var i=0; i < els.length; i++) {
+//     els[i].setAttribute("class", "publish_1");
