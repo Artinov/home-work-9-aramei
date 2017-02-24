7fba63bf20daf7fcce524924d0d0250741a05e14
Artur <ara@mail.com>
2017-02-24 11:05:12 +0200

    completed homework9 using math.random method

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,10 @@
 var ball = document.querySelector(".discoball");
 
-var classes = [".here", ".goes", ".our", ".awesome", ".disco", ".ball"];
+var classes = ["here", "goes", "our", "awesome", "disco", "ball"];
 
 function goDisco() {
-	var random = Math.floor((Math.random() * 5));
-
-	ball.setAttribute("class", classes[random]);
+	var random = Math.floor((Math.random() * classes.length));
+	ball.setAttribute("class", 'discoball ' + classes[random]);
 }
 
 setInterval(goDisco,300);
-
-// var els = document.querySelectorAll('.publish_0');
-// for (var i=0; i < els.length; i++) {
-//     els[i].setAttribute("class", "publish_1");
