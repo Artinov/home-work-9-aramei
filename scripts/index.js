788dc39cd498993aadea88b2643db1d7f7b49bd6
Apas <pasichnyk89@gmail.com>
2017-02-25 16:23:57 +0200

    completed homework with help of Nicolay

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,19 @@ var ball = document.querySelector(".discoball");
 
 var classes = ["here", "goes", "our", "awesome", "disco", "ball"];
 
-function goDisco() {
-	var random = Math.floor((Math.random() * classes.length));
-	ball.setAttribute("class", 'discoball ' + classes[random]);
+var i = 0;
+
+//function goDisco() {
+//	var random = Math.floor((Math.random() * classes.length));
+//	ball.setAttribute("class", "discoball " + classes[random]);
+//}
+
+//setInterval(goDisco,300);
+
+function goDiscoTrue() {
+		ball.setAttribute("class", "discoball " + classes[i]);
+		i++;
+		if (i > 5)	{i = 0;}
 }
 
-setInterval(goDisco,300);
+setInterval(goDiscoTrue,300);
